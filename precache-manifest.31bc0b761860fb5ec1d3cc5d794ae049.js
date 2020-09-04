aae8ef892610d99f6e66d04bd18919e69949b8f8
CodingwithJess <thejessiebess@gmail.com>
2020-09-04 09:37:36 -0600

    Updates

---
+++ precache-manifest.31bc0b761860fb5ec1d3cc5d794ae049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252458633f05301e9de36cd5081cea4e",
+    "url": "/react-directory/index.html"
+  },
+  {
+    "revision": "7738fee91777fc56a90c",
+    "url": "/react-directory/static/css/main.c91e6abe.chunk.css"
+  },
+  {
+    "revision": "48ca80cba27cee3109b7",
+    "url": "/react-directory/static/js/2.697fb560.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-directory/static/js/2.697fb560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7738fee91777fc56a90c",
+    "url": "/react-directory/static/js/main.90746f7f.chunk.js"
+  },
+  {
+    "revision": "deba56ab0f39f48f6548",
+    "url": "/react-directory/static/js/runtime-main.57cf64e2.js"
+  }
+]);
